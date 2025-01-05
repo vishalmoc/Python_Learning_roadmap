86cb41e74ca56044cfa2b8b02e6b52d245a80266
Saurabh Singh Bhandari <saurabhsinghbhandarissb@gmail.com>
2025-01-06 03:44:58 +0530

    Modifying Content for faster understanding

--- 02. Learning about Variables and Data Types.py
+++ 02. Learning about Variables and Data Types.py
@@ -4,7 +4,9 @@
 a1=1
 B1=2
 _c1=3
+
 # not 1c_=4
+
 print( a1, B1, _c1)
 print( "This is type of a1", type( a1))
 
@@ -25,6 +27,7 @@ print("Sequenced Data Type= ",[12,15,16,18]," ",(11,14,17,19))
 print("Dictionary= ", {"name": "Rohan", "age": 25, "Student": True, "Stream": "MBA"})
 
 # Explicit typecasting- Done by user 
+
 string = "15"
 number = 7
 string_number = int(string) # throws an error if the string is not a valid integer
@@ -34,19 +37,23 @@ print("The Sum of both the numbers is: ", sum)
 # Implicit type casting- Done by Python automatically
 
 # Python converts a to int
+
 a = 7
 print(type(a))
 
 # Python converts b to float
+
 b = 3.0
 print(type(b))
 
 # Python converts c to float 
+
 c = a + b
 print(c)
 print(type(c))
 
 # Global and Local Variables
+
 # Variables that are defined inside a function body have a local scope, and those defined outside have a global scope
 
 a = 1
